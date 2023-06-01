b3ef30179049cd1f77e418c3d688051f6c94229d
Krushna Kulkarni <krushnakulkarni17@gmail.com>
2023-06-01 23:41:41 +0530

    Added ProductLsiting Page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,12 +3,14 @@ import React from "react";
 import {Routes, Route} from "react-router-dom"
 import MockMan from "mockman-js";
 import { Home } from "./pages/Home";
+import { ProductListing } from "./pages/ProductListing";
 
 function App() {
   return (
     <div className="App">
       <Routes>
       <Route path="/" element={<Home/>} />
+      <Route path="/products" element={<ProductListing/>} />
       <Route path="/mockman" element={<MockMan/>} />
       </Routes>
  
